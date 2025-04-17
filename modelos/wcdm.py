8fe0ff20218cb64527b3ec37d808f3e1ac1b35da
ismag4 <i17ag4@gmail.com>
2025-04-17 21:36:06 +0200

    Update wcdm.py

--- modelos/wcdm.py
+++ modelos/wcdm.py
@@ -146,14 +146,6 @@ variables = np.array([samples[:,0], samples[:,1], samples[:,3],
                       omega_lambda]).T
 labels = ["h", "omega_m", "w", "omega_lambda"]
 
-
-#guardamos en 2 archivos.txt los valores obtenidos y los valores medios
-encabezado_m = "h_media\th_err\tomega_m\tomega_m_err\tw\tw_err\tomega_lambda\tomega_lambda_err"
-val_medios_err = np.array([h_media, h_er, om_media, om_er, w_media,
-                           w_er, ol_media, ol_er])
-np.savetxt("wcdm_pr_val_medios.txt", val_medios_err, fmt='%.16f',
-           delimiter='\t', header = encabezado_m, comments='')
-
 encabezado_s = "h\tomega_m\tw\tomega_lambda"
 np.savetxt("wcdm_pr_param.txt", variables, fmt='%.16f', delimiter='\t',
            header=encabezado_s, comments='')
