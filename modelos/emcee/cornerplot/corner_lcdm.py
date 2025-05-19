31161d72171124811824f15bbbed258030f5e37e
ismag4 <i17ag4@gmail.com>
2025-05-19 10:04:24 +0200

    Create corner_lcdm.py

---
+++ modelos/emcee/cornerplot/corner_lcdm.py
@@ -0,0 +1,20 @@
+import numpy as np
+from getdist import MCSamples
+from getdist import plots
+import matplotlib.pyplot as plt
+
+h = np.loadtxt('lcdm_param.txt', skiprows = 1, usecols = 0, unpack = False)
+om = np.loadtxt('lcdm_param.txt', skiprows = 1, usecols = 1, unpack = False)
+ol = np.loadtxt('lcdm_param.txt', skiprows = 1, usecols = 2, unpack = False)
+
+samples = np.array([h, om, ol]).T
+names = ['h', 'omega_m', 'omega_lambda']
+labels = ['h', r'\Omega_m', r'\Omega_\Lambda']
+
+mc_samples = MCSamples(samples=samples, names=names, labels=labels)
+
+print(mc_samples.getTable(limit=1).tableTex())
+
+g = plots.get_subplot_plotter()
+g.triangle_plot(mc_samples, filled=True)
+plt.show()
